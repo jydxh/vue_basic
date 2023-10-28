22dc3a57352c13815fa4db05e36bc57ca7d17ac3
jydxh <wzxxds@gmail.com>
2023-10-28 10:17:16 -0400

    day04 加载post请求进行模糊查询

--- day02/demo/vue-project/src/views/Http.vue
+++ day02/demo/vue-project/src/views/Http.vue
@@ -3,6 +3,9 @@
 		<!-- Http.vue 测试http请求 -->
 		<h2>Axios 请求测试</h2>
 		<button @click="getMovies">发送请求，加载电影列表</button>
+		<hr />
+		<input type="text" placeholder="Please Enter The Keywords of a Movie" v-model="name" /> &nbsp;&nbsp;
+		<button @click="queryMovies">submit the post request</button>
 
 		<hr />
 		<h1>查询结果</h1>
@@ -12,7 +15,7 @@
 			<div class="actor">{{ item.star_actor }}</div>
 			<div class="showingon">{{ item.showingon }}</div>
 			<div class="type">{{ item.type }}</div>
-			<div class="duration">{{ item.duration }}hours</div>
+			<div class="duration">{{ item.duration }}minutes</div>
 		</div>
 	</div>
 </template>
@@ -24,6 +27,7 @@
 		data() {
 			return {
 				movie: [],
+				name: "", // use to store the keywords
 			};
 		},
 		methods: {
@@ -37,6 +41,21 @@
 					.then(res => {
 						console.log(res.data.data.result);
 						this.movie = res.data.data.result; // 电影数组保存到了movie中了
+					})
+					.catch(err => {
+						console.log(err);
+					});
+			},
+
+			queryMovies() {
+				// post request for query the movies
+				instance({
+					url: "https://web.codeboy.com/bmdapi/movie-infos/name",
+					method: "POST",
+					data: `name=${this.name}&page=1&pagesize=20`,
+				})
+					.then(res => {
+						console.log(res.data.data.result);
 					})
 					.catch(err => {
 						console.log(err);
@@ -56,13 +75,16 @@
 		align-items: center;
 		border-bottom: 1px solid #ccc;
 		.cover {
-			width: 150px;
+			width: 100px;
+			img {
+				width: 100%;
+			}
 		}
 		.title {
 			flex: 1;
 		}
 		.actor {
-			width: 250px;
+			width: 220px;
 		}
 		.showingon {
 			width: 150px;
