aaebc8de9a99d2860472d502fbe19b3b453a2e1b
jydxh <wzxxds@gmail.com>
2023-10-28 13:02:40 -0400

    用components自定义子组件方式完成了actor 列表

---
+++ day02/demo/vue-project/src/components/Person.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<img :src="avatar" alt="" />
+		<p>{{ name }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		//声明自定义属性，接受父组件传递的参数
+		props: ["avatar", "name"],
+	};
+</script>
+
+<style lang="scss" scoped>
+	.card {
+		border: 1px solid #999;
+		border-radius: 8px;
+		overflow: hidden;
+		margin: 5px;
+		width: 150px;
+		img {
+			width: 100%;
+			border-radius: 8px;
+			object-fit: cover;
+		}
+		> p {
+			text-overflow: ellipsis;
+			overflow: hidden;
+			white-space: nowrap;
+		}
+	}
+</style>
--- day02/demo/vue-project/src/views/Actor.vue
+++ day02/demo/vue-project/src/views/Actor.vue
@@ -4,19 +4,25 @@
 		<hr />
 		<button @click="showActors">click to show first 100 actors</button>
 		<hr />
-		<div class="box">
+		<!-- <div class="box">
 			<div class="card" v-for="item in actor" :key="item.id">
 				<div><img :src="item.actor_avatar" alt="" /></div>
 				<p>{{ item.actor_name }}</p>
 			</div>
+		</div> -->
+		<div class="box">
+			<person class="card" v-for="item in actor" :key="item.id" :avatar="item.actor_avatar" :name="item.actor_name"></person>
 		</div>
 	</div>
 </template>
 
 <script>
+	import Person from "../components/Person.vue"; // 引入子组件 person
 	import Myaxios from "../http/Myaxios";
 
 	export default {
+		// components 用于指明当前组件中 需要用哪些子组件
+		components: { Person },
 		data() {
 			return {
 				actor: [],
@@ -28,8 +34,9 @@
 				const para = { page: 1, pagesize: 100 };
 				Myaxios.get(url, para)
 					.then(res => {
-						console.log(res.data.data);
+						// console.log(res.data.data);
 						this.actor = res.data.data;
+						console.log(this.actor);
 					})
 					.catch(err => {
 						console.log(err);
@@ -40,7 +47,7 @@
 </script>
 
 <style lang="scss" scoped>
-	.box {
+	/* .box {
 		width: 90vw;
 		display: flex;
 		flex-wrap: wrap;
@@ -62,5 +69,12 @@
 				white-space: nowrap;
 			}
 		}
+	} */
+
+	.box {
+		width: 90vw;
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 auto;
 	}
 </style>
